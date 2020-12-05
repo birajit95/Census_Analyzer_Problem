3f4b140559be269d5f16194966c35bc2dae43560
Birajit <birajit95@gmail.com>
2020-12-05 23:19:04 +0530

    [Birajit] Add: State code data is loaded

--- com/bridgelabz/censusanalyzer/CensusAnalyser.py
+++ com/bridgelabz/censusanalyzer/CensusAnalyser.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from com.bridgelabz.censusanalyzer.CensusAnalyserCSVHeader import CensusAnalyserCSVHeader
+from com.bridgelabz.censusanalyzer.CensusAnalyserCSVHeader import StateCensusCSVHeader, StateCodeCSVHeader
 from com.bridgelabz.censusanalyzer.CensusAnalyserException import CensusAnalyserException
 from com.bridgelabz.censusanalyzer.CensusAnalyserException import ExceptionType
 
@@ -7,13 +7,14 @@ from com.bridgelabz.censusanalyzer.CensusAnalyserException import ExceptionType
 class CensusAnalyser:
     def __init__(self):
         self.stateCensusData = None
+        self.stateCodaData = None
 
     def loadStateCensusData(self, path, delimiter="\t", header=None):
         """This function loads state census data and raise exceptions if found any error"""
 
         if header is None:
-            header = repr(CensusAnalyserCSVHeader()).split(',')
-        elif header != repr(CensusAnalyserCSVHeader()).split(','):
+            header = repr(StateCensusCSVHeader()).split(',')
+        elif header != repr(StateCensusCSVHeader()).split(','):
             raise CensusAnalyserException(ExceptionType.INCORRECT_HEADER_EXCEPTION.value)
         if not path.find(".csv"):
             raise CensusAnalyserException(ExceptionType.INCORRECT_FILE_TYPE_EXCEPTION.value)
@@ -28,9 +29,21 @@ class CensusAnalyser:
         """returns length of the data set"""
         return len(self.stateCensusData)
 
+    def loadStateCodeData(self, path, delimiter="\t", header=None):
+        """This function loads State code data set and can raise exception if any error found"""
+
+        if header is None:
+            header = repr(StateCodeCSVHeader()).split(',')
+        self.stateCodaData = pd.read_csv(path, sep=delimiter, skiprows=[0], names=header)
+
+
+
+
 
 if __name__ == '__main__':
     STATE_CENSUS_PATH = r"..\Resource\stateCensusData.csv"
+    STATE_CODE_PATH = r"..\Resource\stateCodeData.csv"
     analyser = CensusAnalyser()
-    analyser.loadStateCensusData(STATE_CENSUS_PATH)
-    # analyser.getStateCensusRecordCount()
+    # analyser.loadStateCensusData(STATE_CENSUS_PATH)
+    analyser.loadStateCodeData(STATE_CODE_PATH)
+    # print(analyser.getStateCensusRecordCount())
