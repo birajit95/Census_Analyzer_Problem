1176778c5484a0732a1d842cfacb0432f5d62b25
Birajit <birajit95@gmail.com>
2020-12-05 19:41:24 +0530

    [Birajit] Add: State Census Total records is counted

--- com/bridgelabz/censusanalyzer/CensusAnalyser.py
+++ com/bridgelabz/censusanalyzer/CensusAnalyser.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from com.bridgelabz.censusanalyzer.CensusAnalyserCSVHeader import CensusAnalyserCSVHeader
 
+
 class CensusAnalyser:
     def __init__(self):
         self.stateCensusData = None
@@ -10,7 +11,12 @@ class CensusAnalyser:
         header = repr(CensusAnalyserCSVHeader()).split(',')
         self.stateCensusData = pd.read_csv(path, sep=delimiter, skiprows=[0], names=header)
 
+    def getStateCensusRecordCount(self):
+        return len(self.stateCensusData)
+
+
 if __name__ == '__main__':
     STATE_CENSUS_PATH = r"..\Resource\stateCensusData.csv"
     analyser = CensusAnalyser()
     analyser.loadStateCensusData(STATE_CENSUS_PATH)
+    analyser.getStateCensusRecordCount()
