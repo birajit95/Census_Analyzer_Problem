06bb3dbea61dd9ff07874b3363fa679645d8a73b
Birajit <birajit95@gmail.com>
2020-12-05 23:13:47 +0530

    [Birajit] Add: State Code CSV header is added

--- com/bridgelabz/censusanalyzer/CensusAnalyserCSVHeader.py
+++ com/bridgelabz/censusanalyzer/CensusAnalyserCSVHeader.py
@@ -1,4 +1,4 @@
-class CensusAnalyserCSVHeader:
+class StateCensusCSVHeader:
     def __init__(self):
         self.state = "State"
         self.population = "Population"
@@ -7,3 +7,14 @@ class CensusAnalyserCSVHeader:
 
     def __repr__(self):
         return f"{self.state},{self.population},{self.area},{self.density}"
+
+
+class StateCodeCSVHeader:
+    def __init__(self):
+        self.serialNo = "SerialNo"
+        self.state = "State"
+        self.tin = "TIN"
+        self.stateCode = "StateCode"
+
+    def __repr__(self):
+        return f"{self.serialNo},{self.state},{self.tin},{self.stateCode}"
