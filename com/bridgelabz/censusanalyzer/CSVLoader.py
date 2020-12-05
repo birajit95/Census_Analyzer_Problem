cc71e2c2ba996c693723c03656e53a0cbdc2f98b
Birajit <birajit95@gmail.com>
2020-12-06 02:01:10 +0530

    [Birajit] Refactor: CSV file loding responsibility has ben deligated to other module

---
+++ com/bridgelabz/censusanalyzer/CSVLoader.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from com.bridgelabz.censusanalyzer.CensusAnalyserCSVHeader import StateCensusCSVHeader, StateCodeCSVHeader
+from com.bridgelabz.censusanalyzer.CensusAnalyserException import CensusAnalyserException
+from com.bridgelabz.censusanalyzer.CensusAnalyserException import ExceptionType
+
+
+class CSVLoader:
+
+    @staticmethod
+    def genericCSVLoader(path, fileName, delimiter="\t", header=None):
+        """This generic loader function can load both the data set and raise exceptions if found any error"""
+
+        if fileName == "StateCodeCSV":
+            if header is None:
+                header = repr(StateCodeCSVHeader()).split(',')
+            elif header != repr(StateCodeCSVHeader()).split(','):
+                raise CensusAnalyserException(ExceptionType.INCORRECT_HEADER_EXCEPTION.value)
+        else:
+            if header is None:
+                header = repr(StateCensusCSVHeader()).split(',')
+            elif header != repr(StateCensusCSVHeader()).split(','):
+                raise CensusAnalyserException(ExceptionType.INCORRECT_HEADER_EXCEPTION.value)
+
+        if not path.find(".csv"):
+            raise CensusAnalyserException(ExceptionType.INCORRECT_FILE_TYPE_EXCEPTION.value)
+
+        if delimiter != "\t":
+            raise CensusAnalyserException(ExceptionType.INCORRECT_DELIMITER_EXCEPTION.value)
+
+        try:
+            return pd.read_csv(path, sep=delimiter, skiprows=[0], names=header)
+        except FileNotFoundError:
+            raise CensusAnalyserException(ExceptionType.WRONG_FILE_PATH_EXCEPTION.value)
--- com/bridgelabz/censusanalyzer/CensusAnalyser.py
+++ com/bridgelabz/censusanalyzer/CensusAnalyser.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from com.bridgelabz.censusanalyzer.CensusAnalyserCSVHeader import StateCensusCSVHeader, StateCodeCSVHeader
-from com.bridgelabz.censusanalyzer.CensusAnalyserException import CensusAnalyserException
-from com.bridgelabz.censusanalyzer.CensusAnalyserException import ExceptionType
+from com.bridgelabz.censusanalyzer.CSVLoader import CSVLoader
 
 
 class CensusAnalyser:
@@ -9,34 +6,9 @@ class CensusAnalyser:
         self.stateCensusData = None
         self.stateCodeData = None
 
-    def genericCSVLoader(self, path, fileName, delimiter="\t", header=None):
-        """This generic loader function loads both the data set and raise exceptions if found any error"""
-        
-        if fileName == "StateCodeCSV":
-            if header is None:
-                header = repr(StateCodeCSVHeader()).split(',')
-            elif header != repr(StateCodeCSVHeader()).split(','):
-                raise CensusAnalyserException(ExceptionType.INCORRECT_HEADER_EXCEPTION.value)
-        else:
-            if header is None:
-                header = repr(StateCensusCSVHeader()).split(',')
-            elif header != repr(StateCensusCSVHeader()).split(','):
-                raise CensusAnalyserException(ExceptionType.INCORRECT_HEADER_EXCEPTION.value)
-
-        if not path.find(".csv"):
-            raise CensusAnalyserException(ExceptionType.INCORRECT_FILE_TYPE_EXCEPTION.value)
-
-        if delimiter != "\t":
-            raise CensusAnalyserException(ExceptionType.INCORRECT_DELIMITER_EXCEPTION.value)
-
-        try:
-            return pd.read_csv(path, sep=delimiter, skiprows=[0], names=header)
-        except FileNotFoundError:
-            raise CensusAnalyserException(ExceptionType.WRONG_FILE_PATH_EXCEPTION.value)
-
     def loadStateCensusData(self, path, delimiter="\t", header=None):
         """This function loads state census data and raise exceptions if found any error"""
-        self.stateCensusData = self.genericCSVLoader(path, "StateCensusCSV", delimiter=delimiter, header=header)
+        self.stateCensusData = CSVLoader.genericCSVLoader(path, "StateCensusCSV", delimiter=delimiter, header=header)
 
     def getStateCensusRecordCount(self):
         """returns length of the data set"""
@@ -44,18 +16,18 @@ class CensusAnalyser:
 
     def loadStateCodeData(self, path, delimiter="\t", header=None):
         """This function loads State code data set and can raise exception if any error found"""
-        self.stateCodeData = self.genericCSVLoader(path, "StateCodeCSV", delimiter=delimiter, header=header)
+        self.stateCodeData = CSVLoader.genericCSVLoader(path, "StateCodeCSV", delimiter=delimiter, header=header)
 
     def getStateCodeRecordCount(self):
         """returns length of the data set"""
         return len(self.stateCodeData)
 
-
-if __name__ == '__main__':
-    STATE_CENSUS_PATH = r"..\Resource\stateCensusData.csv"
-    STATE_CODE_PATH = r"..\Resource\stateCodeData.csv"
-    analyser = CensusAnalyser()
-    analyser.loadStateCensusData(STATE_CENSUS_PATH)
-    analyser.loadStateCodeData(STATE_CODE_PATH)
-    print(analyser.getStateCensusRecordCount())
-    print(analyser.getStateCodeRecordCount())
+#
+# if __name__ == '__main__':
+#     STATE_CENSUS_PATH = r"..\Resource\stateCensusData.csv"
+#     STATE_CODE_PATH = r"..\Resource\stateCodeData.csv"
+#     analyser = CensusAnalyser()
+#     analyser.loadStateCensusData(STATE_CENSUS_PATH)
+#     analyser.loadStateCodeData(STATE_CODE_PATH)
+#     print(analyser.getStateCensusRecordCount())
+#     print(analyser.getStateCodeRecordCount())
